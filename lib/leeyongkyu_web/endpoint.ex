c89d85c20b2748e9e36f2882e08f9e2a554ff439
Taehwan Kim <freewebwithme777@protonmail.com>
2021-08-06 09:23:04 -0700

    First deploy

--- lib/leeyongkyu_web/endpoint.ex
+++ lib/leeyongkyu_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule LeeyongkyuWeb.Endpoint do
   ]
 
   socket "/socket", LeeyongkyuWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
